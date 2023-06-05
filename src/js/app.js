180e43060ad9db9fd20762ce09ac36f7ef2ad84f
jerinhons117 <131413483+jerinhons117@users.noreply.github.com>
2023-06-05 00:59:23 +0000

    modificacion de los links de redes sociales

--- src/js/app.js
+++ src/js/app.js
@@ -38,10 +38,10 @@ function render(variables = {}) {
           <h3>${variables.country}</h3>
           <h4>${variables.city}</h4>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/jerinhons117"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/jerinhons117"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://www.instagram.com/jerindesing/"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://www.instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
